db95cdf1a65c2199af498afd813885e210945f72
Impormptu Versus <plsgivememail123456789101112@gmail.com>
2019-11-28 19:10:09 +0200

    changed app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            We are working on the feature branch!
         </p>
         <a
           className="App-link"
